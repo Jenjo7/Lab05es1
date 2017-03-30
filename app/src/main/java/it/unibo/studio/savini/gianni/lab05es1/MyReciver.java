9f3095a11386b4fc044ab67bd41d46e4f9772d56
Gianni Savini <gianni.savini@studio.unibo.it>
2017-03-30 16:08:07 +0200

    Example of Broadcast class

--- app/src/main/java/it/unibo/studio/savini/gianni/lab05es1/MyReciver.java
+++ app/src/main/java/it/unibo/studio/savini/gianni/lab05es1/MyReciver.java
@@ -3,7 +3,9 @@ package it.unibo.studio.savini.gianni.lab05es1;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
+import android.util.StringBuilderPrinter;
 
 /**
  * Created by gianni.savini on 30/03/2017.
@@ -15,6 +17,24 @@ public class MyReciver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i("MyReciver", "Ricevuto Intent");
+        int resultCode = getResultCode();
+        Log.i("MyReciver", "Ricevuto Intent" + String.valueOf(resultCode));
+        String resultData = getResultData();
+        Log.i("MyReciver", "Ricevuto Intent" + resultData);
+
+        String extra;
+        Bundle bundle = intent.getExtras();
+        if(bundle != null) {
+            extra = bundle.getString(MainActivity.EXTRA);
+            Log.i("MyReciver", extra);
+
+            extra += "- 1";
+
+            bundle.putString(MainActivity.EXTRA, extra);
+            setResultExtras(bundle);
+        }
+
+
         if(intent.hasExtra(MainActivity.EXTRA)) {
             Log.i("MyReciver", "Messaggio: " + intent.getStringExtra(MainActivity.EXTRA));
         }
