387a85695ff072aaae730afbea29f42f236a645f
Gianni Savini <gianni.savini@studio.unibo.it>
2017-03-30 15:44:20 +0200

    I create 3 classes like MyReciver

---
+++ app/src/main/java/it/unibo/studio/savini/gianni/lab05es1/MyReciver2.java
@@ -0,0 +1,40 @@
+package it.unibo.studio.savini.gianni.lab05es1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by gianni.savini on 30/03/2017.
+ */
+public class MyReciver2 extends BroadcastReceiver {
+    public void onReceive(Context context, Intent intent) {
+        Log.i("MyReciver", "Ricevuto Intent");
+        int resultCode = getResultCode();
+        Log.i("MyReciver", "Ricevuto Intent" + String.valueOf(resultCode));
+        String resultData = getResultData();
+        Log.i("MyReciver", "Ricevuto Intent" + resultData);
+
+        setResultCode(2);
+        setResultData("Fonte2");
+
+        String extra;
+        Bundle bundle = intent.getExtras();
+        if(bundle != null) {
+            extra = bundle.getString(MainActivity.EXTRA);
+            Log.i("MyReciver", extra);
+
+            extra += "- 1";
+
+            bundle.putString(MainActivity.EXTRA, extra);
+            setResultExtras(bundle);
+        }
+
+
+        if(intent.hasExtra(MainActivity.EXTRA)) {
+            Log.i("MyReciver", "Messaggio: " + intent.getStringExtra(MainActivity.EXTRA));
+        }
+    }
+}
